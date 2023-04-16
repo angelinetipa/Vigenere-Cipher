5537e3eab027f0d9349a42cf199bdfcc84c3e007
angelinetipa <angelinetipa@gmail.com>
2023-04-17 00:20:48 +0800

    did few changes

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -11,31 +11,31 @@ for j in range(len(message)):
     message_indices.append(alphabet.find(message[j]))
 # list for keyword indices
 keyword_indices = []
-# find the index of each letter in message then add to list
+# find the index of each letter in keyword then add to list
 for i in range(len(keyword)):
     keyword_indices.append(alphabet.find(keyword[i]))
 
 index1 = 0
 index2 = 0
-# list for mod of sum of message's and keyword's indices
-mod_message_keyword = []
+# list for sum of message's and keyword's indices
+sum_message_keyword = []
 # for every message indices a keyword indices will add
 for k in range(len(message_indices)):
     sum = message_indices[index1] + keyword_indices[index2]
 # if sum less than the len of alphabet, add to list, otherwise take mod then add to list
     if sum < len(alphabet):
-        mod_message_keyword.append(sum)
+        sum_message_keyword.append(sum)
     elif sum >= len(alphabet):
-        mod_message_keyword.append(sum % len(alphabet))
+        sum_message_keyword.append(sum % len(alphabet))
     index1 += 1
 # if index2 on last index of keyword, bring it back to zero
     index2 += 1
     if index2 == len(keyword_indices):
         index2 -= len(keyword_indices)
 print("\n\033[3m\033[1m" "Ciphertext: " "\033[0m")     
-# take the index from list mod message keyword to the variable alphabet
-for p in range(len(mod_message_keyword)):
-    mod_indices = alphabet[mod_message_keyword[p]]
+# take the index from list of sum message and keyword to the variable alphabet
+for p in range(len(sum_message_keyword)):
+    mod_indices = alphabet[sum_message_keyword[p]]
 # output for ciphertext/print the result
-    print("\033[4m", mod_indices, end="") 
+    print("\033[4m" + mod_indices, end=" ") 
 print("\033[0m" "\n\n", end="*" * 55)
