bd262c4d092bf0aed05d916189c06446d925e152
angelinetipa <angelinetipa@gmail.com>
2023-04-16 23:09:46 +0800

    initial commit

---
+++ Vigenere Cipher.py
@@ -0,0 +1,12 @@
+# a variable for alphabetical letters
+# input for the plaintext and keyword, remove space and change to uppercase
+# list for message indices
+# find the index of each letter in message then add to list
+# list for keyword indices
+# find the index of each letter in message then add to list
+# list for mod of sum of message's and keyword's indices
+# for every message indices a keyword indices will add
+# if sum less than the len of alphabet, add to list, otherwise take mod then add to list
+# if index2 on last index of keyword, bring it back to zero
+# take the index from list mod message keyword to the variable alphabet
+# output for ciphertext/print the result
