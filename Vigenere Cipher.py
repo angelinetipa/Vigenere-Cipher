1ad3cfb65977741ea77bfe4025763fd0ba36b455
angelinetipa <angelinetipa@gmail.com>
2023-04-16 23:14:37 +0800

    made list then append message indices

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -5,7 +5,10 @@ alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 message = input("\n\033[3m\033[1m" "Message: \n> " "\033[0m").strip().upper()
 keyword = input("\n\033[3m\033[1m" "Key: \n> " "\033[0m").strip().upper()
 # list for message indices
+message_indices = []
 # find the index of each letter in message then add to list
+for j in range(len(message)):
+    message_indices.append(alphabet.find(message[j]))
 # list for keyword indices
 # find the index of each letter in message then add to list
 # list for mod of sum of message's and keyword's indices
